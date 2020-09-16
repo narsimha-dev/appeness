9550a544e01e5f165ee9bda2b07e77f12ad47840
Narasimhulu <44318990+narsimha-dev@users.noreply.github.com>
2020-09-16 10:54:43 +0530

    React hook exampl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from 'react';
 import {Main} from './components/Main';
 import NavbarEmp from './components/NavbarEmp';
-
+import CounterWithNameAndSideEffect from './components/CounterWithNameAndSideEffect ';
 export const App=()=> {
   return <Fragment>
-    <NavbarEmp/>
-      <Main/>
+    {/* <NavbarEmp/> */}
+      {/* <Main/> */}
+      <CounterWithNameAndSideEffect/> 
       </Fragment>
 }
---
+++ src/components/CounterWithNameAndSideEffect .js
@@ -0,0 +1,25 @@
+import React from 'react';
+const { useEffect, useState } = React
+
+const CounterWithNameAndSideEffect = () => {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('Flavio')
+
+  useEffect(() => {
+    console.log(`Hi ${name} you clicked ${count} times`)
+  })
+
+  return (
+    <div>
+      <p>
+        Hi {name} you clicked {count} times
+      </p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setName(name === 'Flavio' ? 'Roger' : 'Flavio')}>
+        Change name
+      </button>
+    </div>
+  )
+}
+
+export default CounterWithNameAndSideEffect;
